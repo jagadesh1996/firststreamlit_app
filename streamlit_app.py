2c4bd3b69db210fdc1bd8cf2506844777cc40a4b
Sai Jagadeeswar <121271438+jagadesh1996@users.noreply.github.com>
2023-09-06 00:51:09 +0530

    Update streamlit_app.py
    small changes in syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('Hotel HAJ')
 
 streamlit.header('Breakfast Menu🥣')
-streamlit.text(🥗--'IDLY, POORI, DOSA, VADA, PONGAL')
-streamlit.text(🍞--'Boiled EGG, TEA, COFFEE, BISCUITS...')
-streamlit.text(🐔--'Biryani, Wings, 555....')
+streamlit.text('🥗 IDLY, POORI, DOSA, VADA, PONGAL')
+streamlit.text('🍞 Boiled EGG, TEA, COFFEE, BISCUITS...')
+streamlit.text('🐔 Biryani, Wings, 555....')
 
